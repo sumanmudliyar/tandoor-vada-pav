58addead728db2a1ec6df39197e46479083a57c1
sumanmudliyar35 <suman.mudliyar@spit.ac.in>
2026-02-15 11:00:52 +0530

    removed image

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,17 +1,14 @@
 type Props = {
   name: string;
   price: number;
-  image: string;
 };
 
 const FoodCard: React.FC<Props> = ({
   name,
   price,
-  image,
 }) => {
   return (
     <div className="card">
-      <img src={image} alt={name} />
       <h3>{name}</h3>
       <p>₹{price}</p>
     </div>
